d4463ff0df1a6d3541380f532cce1d3eba332c8f
Sem Rekkers <rekkers.sem@gmail.com>
2017-06-19 12:09:16 +0200

    stream: Move towards new architecture

--- stream/stream.go
+++ stream/stream.go
@@ -3,61 +3,46 @@ package stream
 import (
 	"fmt"
 	"net"
-	"os"
-	"path/filepath"
 
 	"github.com/thesquare-avans/StreamService/fsd"
 )
 
 type Server struct {
-	l        net.Listener
-	fragment int
-	dir      string
-	conn     net.Conn
-	parser   *fsd.Parser
+	l    *net.TCPListener
+	pipe chan *fsd.Fragment
 }
 
-func NewServer(laddr, dir string) (*Server, error) {
-	l, err := net.Listen("tcp", laddr)
+func NewServer(host string, port int, pipe chan *fsd.Fragment) (*Server, error) {
+	addr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", host, port))
 	if err != nil {
 		return nil, err
 	}
-	return &Server{l: l, dir: dir}, nil
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+	return &Server{l: l, pipe: pipe}, nil
 }
 
-func (s *Server) WaitForClient() error {
-	conn, err := s.l.Accept()
+func (s *Server) Run() error {
+	client, err := s.l.AcceptTCP()
 	if err != nil {
 		return err
 	}
-	s.conn = conn
-	s.parser = fsd.NewParser(s.conn)
-	return err
-}
+	defer client.Close()
+	// TODO: eventually close every other connection
 
-func (s *Server) ReceiveSingle() (int, error) {
-	fragment, err := s.parser.ParseFragment()
-	if err != nil {
-		return 0, err
+	parser := fsd.NewParser(client)
+	for {
+		frag, err := parser.ParseFragment()
+		if err != nil {
+			return err
+		}
+		s.pipe <- frag
 	}
-
-	file := fmt.Sprintf("StreamService-%d.mp4", s.fragment)
-	s.fragment++
-	fullPath := filepath.Join(s.dir, file)
-	fd, err := os.Create(fullPath)
-	if err != nil {
-		return 0, err
-	}
-	defer fd.Close()
-
-	_, err = fd.Write(fragment.Data)
-	return len(fragment.Data), err
 }
 
 func (s *Server) Close() error {
-	err := s.conn.Close()
-	if err != nil {
-		return err
-	}
+	close(s.pipe)
 	return s.l.Close()
 }
